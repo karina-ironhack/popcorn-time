129bf96f699a8e8846de1f97a75af38473125bd3
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2022-02-22 15:48:37 +0200

    add image, genres and popular movies

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ function Main() {
     return(
         <section>
         {moviesArray.map(movie => {
-            return <Movie key={movie.id} title={movie.title} year={movie.year} rating={movie.rating}/>
+            return <Movie key={movie.id} title={movie.title} year={movie.year} genres={movie.genres} rating={movie.rating} image={movie.imgURL}/>
         })}
         </section>
     )
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,19 @@
 import './Movie.css'
 
 function Movie(props) {
+
     return(
-        <div className="movie">
+        <div className={props.rating > 8 ? 'movie popular' : 'movie'}>
+        <img src={props.image} alt={props.title} />
+        <div >
             <h2>{props.title}</h2>
             <p>
                 Year: {props.year} <br />
                 Rating: {props.rating}  <br />
             </p>
+            <p>Genres:  {props.genres.map(gen => <span>{gen} </span>)} </p>
+        </div>
         </div>
     )
 }
-
 export default Movie;
